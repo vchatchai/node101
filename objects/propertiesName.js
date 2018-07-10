af326cb2f689944d7661fba3625deddb730841e6
Chatchai Vichai <ee56054@gmail.com>
2018-07-10 15:17:35 +0700

    add objects

---
+++ objects/propertiesName.js
@@ -0,0 +1,7 @@
+
+var person = {'suffix': "Mr", ["last name"]: "Vichai"}, lastname = "lastname"
+person['first name'] = "Chatchai"
+person[lastname] = 'Vichai'
+console.log(person["first name"])
+console.log(person[lastname])
+console.log(person)
