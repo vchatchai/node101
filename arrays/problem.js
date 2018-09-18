795df1e6be323b05895bb097e72f0a6340ad4540
vchatchai <ee56054@gmail.com>
2018-09-18 15:09:27 +0700

    add array

---
+++ arrays/problem.js
@@ -0,0 +1,13 @@
+let colors = ['red', "green", 'blue'];
+console.log(colors.length);
+
+colors[3] = "black";
+
+console.log(colors.length);
+console.log(colors[3]);
+
+colors.length = 2;
+console.log(colors.length);
+console.log(colors[3]);
+console.log(colors[2]);
+console.log(colors[1]);
