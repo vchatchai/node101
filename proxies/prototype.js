34d9d98ee3fda967b1400b4ad2ca2b4b29a6a72a
Chatchai <ee56054@gmail.com>
2018-09-19 21:48:28 +0700

    add get and set prototype

--- proxies/prototype.js
+++ proxies/prototype.js
@@ -2,8 +2,22 @@ let target = { };
 let proxy = new Proxy(target, {
     getPrototypeOf(trapTarget)  {
         return null;
+
+        // Reflect.getPrototypeOf
     },
     setPrototypeOf(trapTarget, proto) {
         return false;
     }
 });
+
+let targetProto = Object.getPrototypeOf(target);
+let proxyProto = Object.getPrototypeOf(proxy);
+
+console.log(target === Object.prototype);
+console.log(proxyProto === Object.prototype);
+
+//succeeds
+Object.setPrototypeOf(target, {});
+
+//throws error
+Object.setPrototypeOf(proxy, {});
