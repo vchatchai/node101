77a2c082a690d903248e55f32c9b68ca42554dd5
vchatchai <ee56054@gmail.com>
2018-09-21 14:07:13 +0700

    add proxy prototype

--- proxies/prototype.js
+++ proxies/prototype.js
@@ -1,4 +1,27 @@
 let target = { };
+
+
+let proxy2 = new Proxy(target, {
+    getPrototypeOf(trapTarget){
+        return Reflect.getPrototypeOf(trapTarget)
+    },
+    setPrototypeOf(trapTarget, proto){
+        return Reflect.setPrototypeOf(trapTarget, proto)
+    }
+
+});
+
+let targetProto = Object.getPrototypeOf(target);
+let proxyProto = Object.getPrototypeOf(proxy2);
+
+
+console.log(targetProto === Object.prototype);
+console.log(proxyProto === Object.prototype);
+
+Object.setPrototypeOf(target, {});
+Object.setPrototypeOf(proxy2,{});
+
+
 let proxy = new Proxy(target, {
     getPrototypeOf(trapTarget)  {
         return null;
@@ -10,8 +33,8 @@ let proxy = new Proxy(target, {
     }
 });
 
-let targetProto = Object.getPrototypeOf(target);
-let proxyProto = Object.getPrototypeOf(proxy);
+targetProto = Object.getPrototypeOf(target);
+proxyProto = Object.getPrototypeOf(proxy);
 
 console.log(target === Object.prototype);
 console.log(proxyProto === Object.prototype);
